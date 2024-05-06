1ac3801f69fd23d5e4022a68c717cb08e36b6a34
CoderConsume <epicureantechnologies@gmail.com>
2024-05-06 13:06:54 -0400

    js changes

--- SBA316/shake8.js
+++ SBA316/shake8.js
@@ -11,6 +11,14 @@ const startGame = () => {
   questionAsked.innerHTML = userInput;
   userQuestion.append(questionAsked);
 
+  document.getElementById("startGame").addEventListener("click", handleClick);
+
+  function getRandomAnimation() {
+    let animations = ["slideInFromLeft", "fadeIn", "rotate"];
+    let randomIndex = Math.floor(Math.random() * animations.length);
+    return animations[randomIndex];
+  }
+
   chooseAnswer();
 };
 
@@ -21,4 +29,64 @@ const changeClassesDuring = () => {
   document.getElementById("firstImage").classList.add("eightBallShake");
   document.getElementById("secondImage").classList.add("eightBall2");
   document.getElementById("firstAnswer").classList.add("theAnswer");
+};
+
+const changeClassesStart = () => {
+  document.getElementById("firstImage").classList.add("eightBall1");
+  document.getElementById("secondImage").classList.add("hiddenEightBall");
+  document.getElementById("firstAnswer").classList.add("hiddenAnswer");
+  document.getElementById("firstImage").classList.remove("eightBallShake");
+  document.getElementById("secondImage").classList.remove("eightBall2");
+  document.getElementById("firstAnswer").classList.remove("theAnswer");
+};
+
+const chooseAnswer = () => {
+  let randomNumber = Math.floor(Math.random() * (21 - 1) + 1);
+  console.log(randomNumber);
+  changeClassesDuring();
+  let theAnswer = document.querySelector(".theAnswer");
+
+  theAnswer.textContent = "";
+
+  if (randomNumber === 1) {
+    theAnswer.append(`It is Certain`);
+  } else if (randomNumber === 2) {
+    theAnswer.append(`It is decidedly so`);
+  } else if (randomNumber === 3) {
+    theAnswer.append(`Without a doubt`);
+  } else if (randomNumber === 4) {
+    theAnswer.append(`Yes definitely`);
+  } else if (randomNumber === 5) {
+    theAnswer.append(`You may rely on it`);
+  } else if (randomNumber === 6) {
+    theAnswer.append(`As I see it, yes`);
+  } else if (randomNumber === 7) {
+    theAnswer.append(`Most likely`);
+  } else if (randomNumber === 8) {
+    theAnswer.append(`Outlook good`);
+  } else if (randomNumber === 9) {
+    theAnswer.append(`Yes`);
+  } else if (randomNumber === 10) {
+    theAnswer.append(`Signs point to yes`);
+  } else if (randomNumber === 11) {
+    theAnswer.append(`Reply hazy, try again`);
+  } else if (randomNumber === 12) {
+    theAnswer.append(`Ask again later`);
+  } else if (randomNumber === 13) {
+    theAnswer.append(`Better not tell you now`);
+  } else if (randomNumber === 14) {
+    theAnswer.append(`Cannot predict now`);
+  } else if (randomNumber === 15) {
+    theAnswer.append(`Concentrate and ask again`);
+  } else if (randomNumber === 16) {
+    theAnswer.append(`Don't count on it`);
+  } else if (randomNumber === 17) {
+    theAnswer.append(`My reply is no`);
+  } else if (randomNumber === 18) {
+    theAnswer.append(`My sources say no`);
+  } else if (randomNumber === 19) {
+    theAnswer.append(`Outlook not so good`);
+  } else {
+    theAnswer.append(`Very Doubtful`);
+  }
 };
